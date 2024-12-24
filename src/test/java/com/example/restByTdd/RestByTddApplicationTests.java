27af37e1e983c01ac8d5c8ad8912e4de25d807c9
Gyeonghun Min <riverpower6@naver.com>
2024-12-24 12:56:40 +0900

    setting

---
+++ src/test/java/com/example/restByTdd/RestByTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restByTdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestByTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
