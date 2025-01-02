aaf75f098f557dc1a0dbf048f69e036f7a40b045
Gyeonghun Min <riverpower6@naver.com>
2025-01-02 12:37:04 +0900

    22: add: PostController - getItem , testcase - getItem

---
+++ src/main/java/com/example/restByTdd/domain/post/post/controller/ApiV1PostController.java
@@ -0,0 +1,21 @@
+package com.example.restByTdd.domain.post.post.controller;
+
+import com.example.restByTdd.domain.post.post.dto.PostDto;
+import com.example.restByTdd.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class ApiV1PostController {
+    private final PostService postService;
+
+    @GetMapping("/{id}")
+    public PostDto item(@PathVariable long id) {
+        return new PostDto(postService.findById(id).get());
+    }
+}
---
+++ src/test/java/com/example/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.restByTdd.domain.post.post.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.restByTdd.domain.post.post.entity.Post;
+import com.example.restByTdd.domain.post.post.service.PostService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class ApiV1PostControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private PostService postService;
+
+    @Test
+    @DisplayName("1번글 조회")
+    void t1() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/posts/1")
+                )
+                .andDo(print());
+
+        Post post = postService.findById(1).get();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("item"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(post.getId()))
+                .andExpect(jsonPath("$.createDate").value(
+                        Matchers.startsWith(post.getCreateDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.modifyDate").value(
+                        Matchers.startsWith(post.getModifyDate().toString().substring(0, 25))))
+                .andExpect(jsonPath("$.authorId").value(post.getAuthor().getId()))
+                .andExpect(jsonPath("$.authorName").value(post.getAuthor().getName()))
+                .andExpect(jsonPath("$.title").value(post.getTitle()))
+                .andExpect(jsonPath("$.content").value(post.getContent()));
+    }
+}
