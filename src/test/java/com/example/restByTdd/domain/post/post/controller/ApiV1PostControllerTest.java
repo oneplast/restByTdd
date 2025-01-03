c980bca123f6a3f11deaa19c5ae2b1302b326536
Gyeonghun Min <riverpower6@naver.com>
2025-01-03 10:14:02 +0900

    29: refactor: testcase - add without input data

--- src/test/java/com/example/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/example/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -218,6 +218,37 @@ public class ApiV1PostControllerTest {
                 .andExpect(jsonPath("$.data.authorName").value(post.getAuthor().getName()))
                 .andExpect(jsonPath("$.data.title").value("축구 하실 분 계신가요?"))
                 .andExpect(jsonPath("$.data.content").value("14시 까지 22명을 모아야 진행이 됩니다."));
+    }
 
+    @Test
+    @DisplayName("글 수정, with no input")
+    void t7() throws Exception {
+        Member actor = memberService.findByUsername("user1").get();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        put("/api/v1/posts/1")
+                                .header("Authorization", "Bearer " + actor.getApiKey())
+                                .content("""
+                                        {
+                                            "title": "",
+                                            "content": ""
+                                        }
+                                        """)
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("modify"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("""
+                        content-Length-length must be between 2 and 10000000
+                        content-NotBlank-must not be blank
+                        title-Length-length must be between 2 and 100
+                        title-NotBlank-must not be blank
+                        """.stripIndent().trim()));
     }
 }
